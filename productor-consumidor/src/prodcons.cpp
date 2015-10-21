151987dcc356459c20a435b3ea614ea33e7537f6
dave95 <davidegallitelli@gmail.com>
2015-10-21 18:59:13 +0200

    # COMMIT 3 # 21102015 #
    Updated with precompiled from course website

--- productor-consumidor/src/prodcons.cpp
+++ productor-consumidor/src/prodcons.cpp
@@ -1,19 +1,66 @@
-/* 
- * File:   main.cpp
- * Author: dave95
- *
- * Created on 14 ottobre 2015, 18.36
- */
-
-#include <cstdlib>
-#include <thread>
-#include <stdio.h>
+#include <iostream>
+#include <cassert>
+#include <pthread.h>
 #include <semaphore.h>
 
-using namespace std;
+using namespace std ;
 
-int main(){
+// ---------------------------------------------------------------------
+// constantes 
+const unsigned
+        num_items  = 40 ,
+        tam_vector = 10 ;
 
-    return 0;
+// ---------------------------------------------------------------------
 
+unsigned producir_dato()
+{
+    static int contador = 0 ;
+    cout << "producido: " << contador << endl << flush ;
+    return contador++ ;
+}
+// ---------------------------------------------------------------------
+
+void consumir_dato( int dato )
+{
+    cout << "dato recibido: " << dato << endl ;
+}
+// ---------------------------------------------------------------------
+
+void * productor( void * )
+{
+    for( unsigned i = 0 ; i < num_items ; i++ )
+    {
+        int dato = producir_dato() ;
+
+        // falta: insertar "dato" en el vector
+        // ................
+
+    }
+    return NULL ;
+}
+// ---------------------------------------------------------------------
+
+void * consumidor( void * )
+{
+    for( unsigned i = 0 ; i < num_items ; i++ )
+    {
+        int dato ;
+
+        // falta: leer "dato" desde el vector intermedio
+        // .................
+
+        consumir_dato( dato ) ;
+    }
+    return NULL ;
+}
+//----------------------------------------------------------------------
+
+int main()
+{
+
+    // falta: crear y poner en marcha las hebras, esperar que terminen
+    // ....
+
+    return 0 ;
 }
