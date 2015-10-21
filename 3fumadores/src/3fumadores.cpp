c5ebfd98084cfc55ac7f7c789d70946aabade569
dave95 <davidegallitelli@gmail.com>
2015-10-21 18:03:11 +0200

    # COMMIT 1 # 21102015 #
    Initial commit:
    - New Project: productor-consumidor
    - New Project: 3fumadores
    - Copied from old Netbeans files

---
+++ 3fumadores/src/3fumadores.cpp
@@ -0,0 +1,60 @@
+/* 
+ * File:   main.cpp
+ * Author: dave95
+ *
+ * Created on 14 ottobre 2015, 18.55
+ */
+
+#include <cstdlib>
+#include <time.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <semaphore.h>
+
+using namespace std;
+
+const int numPersone = 100;
+sem_t estanquero;
+sem_t fumador[3];
+sem_t cerillas, tabaco, papel, objRec;
+
+sem_t tomaAlgo(int);
+void* procFumador(sem_t);
+void* procEstanq();
+void fumar();
+
+int main() {
+    srand(time(NULL));
+    return 0;
+}
+
+void fumar(){
+    const unsigned miliseg = 100U + (rand()%1900U);
+    usleep(1000U*miliseg);
+}
+
+sem_t tomaAlgo(int randNumb){
+    switch (randNumb)
+            case 0:
+                return cerillas;
+            case 1:
+                return tabaco;
+            case 2:
+                return papel;
+}
+
+void* procFumador(sem_t semaforo){ //(*)
+    sem_wait(semaforo);
+    sem_post(objRec);
+    fumar();
+    sem_wait();
+}
+
+void* procEstanq(){
+    int randNumb = rand % 3;
+    sem_post(tomaAlgo(randNumb));
+    sem_wait(objRec);
+}
+
+//ARRAY DI SEMAFORI!!!
+//Meglio passare un intero (vedi seminario) che passare un semaforo (*)
---
+++ productor-consumidor/src/prodcons.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: dave95
+ *
+ * Created on 14 ottobre 2015, 18.36
+ */
+
+#include <cstdlib>
+#include <thread>
+#include <stdio.h>
+#include <semaphore.h>
+
+using namespace std;
+
+int main(){
+
+    return 0;
+
+}
