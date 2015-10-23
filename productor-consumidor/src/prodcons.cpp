90f633ce5df873f005ef6ac0b431cb4bfeddf712
dave95 <davidegallitelli@gmail.com>
2015-10-23 13:09:37 +0200

    # COMMIT 5 # 23102015 #
    Added examples from the first Seminario
    -> Hebras POSIX C++
    Small modifications to producer-consumer (mot working yet)

--- productor-consumidor/src/prodcons.cpp
+++ productor-consumidor/src/prodcons.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cassert>
 #include <pthread.h>
 #include <semaphore.h>
 
@@ -61,6 +60,25 @@ int main()
 
     // falta: crear y poner en marcha las hebras, esperar que terminen
     // ....
+
+    // ==================================
+    int my_vector[tam_vector] = {};
+// ==================================
+
+    pthread_t hebra_productor[num_items], hebra_consumidor[num_items];
+
+    for (unsigned int i = 0; i < num_items; i++){
+        void* arg_ptr = (void*) i;
+        pthread_create(&hebra_productor[i], NULL, productor(), NULL);
+        pthread_create(&hebra_consumidor[i], NULL, consumidor(), NULL);
+    }
+
+    for (unsigned int i=0;i<num_items;i++){
+        pthread_join(&hebra_productor[i],NULL);
+        pthread_join(&hebra_consumidor[i],NULL);
+    }
+
+    pthread_exit (NULL);
 
     return 0 ;
 }
